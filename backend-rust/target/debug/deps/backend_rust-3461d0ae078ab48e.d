010bfc9b8db4387b1c68303d163c885f051bd33b
DanielZanad <daniel.zanad.dz@gmail.com>
2023-04-04 15:23:10 -0300

    add: backend-rust

---
+++ backend-rust/target/debug/deps/backend_rust-3461d0ae078ab48e.d
@@ -0,0 +1,5 @@
+/home/samsepi0l/documents/projects/movie-review-calendar/backend-rust/target/debug/deps/backend_rust-3461d0ae078ab48e.rmeta: src/main.rs
+
+/home/samsepi0l/documents/projects/movie-review-calendar/backend-rust/target/debug/deps/backend_rust-3461d0ae078ab48e.d: src/main.rs
+
+src/main.rs:
---
+++ backend-rust/target/debug/deps/backend_rust-5ee2fa25ba6f299c.d
@@ -0,0 +1,5 @@
+/home/samsepi0l/documents/projects/movie-review-calendar/backend-rust/target/debug/deps/backend_rust-5ee2fa25ba6f299c.rmeta: src/main.rs
+
+/home/samsepi0l/documents/projects/movie-review-calendar/backend-rust/target/debug/deps/backend_rust-5ee2fa25ba6f299c.d: src/main.rs
+
+src/main.rs:
