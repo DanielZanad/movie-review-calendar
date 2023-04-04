05ae7c861688f134b1091ff4ab83aed38c560a40
Daniel Zanad <daniel.zanad.dz@gmail.com>
2023-04-04 14:05:02 -0300

    add: error handling for create route of review-controller

--- backend-nestjs/src/infra/http/controllers/review.controller.ts
+++ backend-nestjs/src/infra/http/controllers/review.controller.ts
@@ -1,12 +1,20 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
 import { PrismaService } from '../../database/prisma/prisma.service';
 import { createReviewBody } from '../dtos/create-review-body';
 import { CreateReview } from '@app/usecases/create-review';
 import { DateReview } from '@app/entities/dateReview';
 import { ReviewViewModel } from '../view-models/review-view-model';
+import { log } from 'console';
 
 @Controller('review')
-export class AppController {
+export class ReviewController {
   constructor(
     private readonly prisma_service: PrismaService,
     private readonly createReview: CreateReview,
@@ -21,15 +29,31 @@ export class AppController {
   async create(@Body() body: createReviewBody) {
     const { content, movie_name } = body;
     const date = new Date(body.date);
-    const { newReview } = await this.createReview.execute({
-      content,
-      date: new DateReview(date),
-      movie_name,
-      reviewed: false,
-    });
 
-    return {
-      review: ReviewViewModel.toHTTP(newReview),
-    };
+    try {
+      const { newReview } = await this.createReview.execute({
+        content,
+        date: new DateReview(date),
+        movie_name,
+        reviewed: false,
+      });
+
+      return {
+        review: ReviewViewModel.toHTTP(newReview),
+      };
+    } catch (error) {
+      console.log(error);
+
+      throw new HttpException(
+        {
+          status: HttpStatus.UNPROCESSABLE_ENTITY,
+          error: error.message,
+        },
+        HttpStatus.UNPROCESSABLE_ENTITY,
+        {
+          cause: error,
+        },
+      );
+    }
   }
 }
--- backend-nestjs/src/infra/http/http.module.ts
+++ backend-nestjs/src/infra/http/http.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
-import { AppController } from './controllers/app.controller';
+import { ReviewController } from './controllers/review.controller';
 import { PrismaService } from '../database/prisma/prisma.service';
 import { CreateReview } from '@app/usecases/create-review';
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [AppController],
+  controllers: [ReviewController],
   providers: [PrismaService, CreateReview],
 })
 export class HttpModule {}
